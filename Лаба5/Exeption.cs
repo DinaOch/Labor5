be87d4cf53ee6b0b47450740858b458a94048417
wakfu <wakfu@DESKTOP-0D21SDO>
2024-06-07 23:25:28 +0300

    Добавьте файлы проекта.

---
+++ Лаба5/Exeption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаба5
+{
+    public class StringTooShortException : Exception
+    {
+        public StringTooShortException(string? message) : base(message)
+        {
+        }
+    }
+}
---
+++ Лаба5/Program.cs
@@ -0,0 +1,36 @@
+﻿using Лаба5;
+
+try
+{
+    // Запросить имя файла и строку для записи
+    Console.Write("Введите имя файла: ");
+    string fileName = Console.ReadLine();
+    Console.Write("Введите строку для записи: ");
+    string inputString = Console.ReadLine();
+
+    // Проверить, что строка содержит как минимум 3 символа
+    if (inputString.Length < 3)
+    {
+        throw new StringTooShortException("Строка слишком мала.");
+    }
+
+    // Записать третий символ строки в файл
+    using (StreamWriter writer = new StreamWriter(fileName))
+    {
+        writer.Write(inputString[2]);
+    }
+
+    Console.WriteLine("Данные успешно записаны в файл.");
+}
+catch (StringTooShortException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Произошло исключение: {ex.Message}");
+}
+finally
+{
+    Console.WriteLine("Выполнение программы завершено.");
+}
